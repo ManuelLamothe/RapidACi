ef7f744ae14bbd70c1ccabe9202a8afb3dbe2385
Manuel Lamothe <Manuel.Lamothe@Canada.ca>
2019-10-03 15:24:36 -0400

    Fixed an exception that did not retrieve dark Rd when leaf area was missing

--- R/correct_dark_fct.r
+++ R/correct_dark_fct.r
@@ -1,15 +1,20 @@
 #' correct_dark function
 #'
-#' @param darkfile_path 
-#' @param leafArea 
-#' @param LiCor_system 
+#' @description Internal function to retrieve the *Photo_out* value measures in dark
+#'   chamber (with the correction for the leaf area if available). This function can be
+#'   used for both the LI6800 and LI6400 generated
 #'
-#' @return A subset dataframe with the variables given in arguments
+#' @param darkfile_path Path to a DARK file
+#' @param leafArea_df A dataframe containing at least a "sample_ID" column and a
+#'   "leafArea_mm2" column (default = NULL)
+#' @param LiCor_system The model of Li-Cor portable photosynthesis system (either "LI6800"
+#'   or "LI6400")
 #'
-#' @export
+#' @return The single value of *Photo_out*
+
 
 correct_dark <- function(darkfile_path, 
-                         leafArea,
+                         leafArea_df,
                          LiCor_system) {
   
   wb <- XLConnect::loadWorkbook(darkfile_path)
@@ -19,41 +24,47 @@ correct_dark <- function(darkfile_path,
     nmA <- readWorksheet(wb, sheet = 1, startRow = 15, endRow = 15, header = FALSE)
     nmG <- paste0(nmB, "_", nmA)
     variables = c("GasEx_A", "Const_S")
+    startRow = 17
   } else if(grepl("6400", LiCor_system)){
     nmB <- readWorksheet(wb, sheet = 1, startRow = 8, endRow = 8, header = FALSE) 
     nmA <- readWorksheet(wb, sheet = 1, startRow = 9, endRow = 9, header = FALSE)
     nmG <- paste0(nmB, "_", nmA)
     variables = c("Photo_out", "Area_in")
+    startRow = 10
   }
   
   if(grepl("6800", LiCor_system)){
-  message("##### Be careful, this function has never been tested with a LI6800. It assumes the the first UserDefVar column was used for sample_ID")
-  message("check printed output")
+  message("##### Be careful, this function assumes the first UserDefVar column was used for sample_ID")
   
-    if(!is.null(leafArea)) {
+    if(!is.null(leafArea_df)) {
       y <- readWorksheet(wb, sheet = 1, startCol = grep("UserDefVar", nmG)[1], 
-                         header = FALSE,  endCol = grep("UserDefVar", nmG)[1])
-      z <- dplyr::filter(leafArea, sample_ID %in% y[,])
+                         header = FALSE,  endCol = grep("UserDefVar", nmG)[1],
+                         startRow = startRow)
+      z <- dplyr::filter(leafArea_df, sample_ID %in% y[,])
       
       line <- vector("list", length = nrow(z))    
       for(i in 1:nrow(z)){ 
         x <- which(y[,] == z$sample_ID[i])
-        writeWorksheet(wb, data = z[i, "leafArea_mm2"] / 100, sheet = 1, 
-                       startRow = x, startCol = which(nmG == variables[2]), header = FALSE)
+        
+        if(!is.na(z[i, "leafArea_mm2"])) {
+          writeWorksheet(wb, data = z[i, "leafArea_mm2"] / 100, sheet = 1, 
+                         startRow = x, startCol = which(nmG == variables[2]), header = FALSE)
+          setForceFormulaRecalculation(wb, sheet = 1, TRUE)
+        }
         
         line[[i]][1] <- z$sample_ID[i]
         line[[i]][2] <- readWorksheet(wb, sheet = 1, header = FALSE,
-                                   startRow = x, endRow = x,
+                                   startRow = x + startRow -1, endRow = x + startRow -1,
                                    startCol = which(nmG == variables[1]), 
                                    endCol = which(nmG == variables[1]))
       }
-      Photo <- do.call(rbind, line) %>% as_tibble() %>% unnest() %>%
+      Photo <- do.call(rbind, line) %>% as_tibble() %>% unnest(cols = c(V1, V2)) %>%
                set_names(c("sample_ID", "Rd"))
       xlcFreeMemory()
       
     } else {
       Photo <- readWorksheet(wb, sheet = 1, header = FALSE,
-                             startRow = 17,
+                             startRow = startRow,
                              startCol = grep("UserDefVar", nmG)[1], 
                              endCol = which(nmG == variables[1])) %>%
                select(1, length(.)) %>%
@@ -62,29 +73,33 @@ correct_dark <- function(darkfile_path,
 
   } else if(grepl("6400", LiCor_system)){
     
-    if(!is.null(leafArea)) {
-      y <- readWorksheet(wb, sheet = 1, startCol = 3, endCol = 3, header = FALSE)
-      z <- dplyr::filter(leafArea, sample_ID %in% y[,])
+    if(!is.null(leafArea_df)) {
+      y <- readWorksheet(wb, sheet = 1, startCol = 3, endCol = 3, startRow = startRow, header = FALSE)
+      z <- dplyr::filter(leafArea_df, sample_ID %in% y[,])
     
       line <- vector("list", length = nrow(z))    
       for(i in 1:nrow(z)){ 
         x <- which(y[,] == z$sample_ID[i])
-        writeWorksheet(wb, data = z[i, "leafArea_mm2"] / 100, sheet = 1, 
-                       startRow = x, startCol = which(nmG == variables[2]), header = FALSE)
-
+        
+        if(!is.na(z[i, "leafArea_mm2"])) {
+          writeWorksheet(wb, data = z[i, "leafArea_mm2"] / 100, sheet = 1, header = FALSE,
+                         startRow = x + startRow -1, startCol = which(nmG == variables[2]))
+          setForceFormulaRecalculation(wb, sheet = 1, TRUE)
+        }
+        
         line[[i]][1] <- z$sample_ID[i]
         line[[i]][2] <- readWorksheet(wb, sheet = 1, header = FALSE,
-                                   startRow = x, endRow = x,
-                                   startCol = which(nmG == variables[1]), 
-                                   endCol = which(nmG == variables[1]))
+                                      startRow = x + startRow -1, endRow = x + startRow -1,
+                                      startCol = which(nmG == variables[1]), 
+                                      endCol = which(nmG == variables[1]))
       }
-      Photo <- do.call(rbind, line) %>% as_tibble() %>% unnest() %>% 
+      Photo <- do.call(rbind, line) %>% as_tibble() %>% unnest(cols = c(V1, V2)) %>% 
                set_names(c("sample_ID", "Rd"))
       xlcFreeMemory()
     
     } else {
       Photo <- readWorksheet(wb, sheet = 1, header = FALSE,
-                             startRow = 10,
+                             startRow = startRow,
                              startCol = 3, 
                              endCol = which(nmG == variables[1])) %>%
                select(1, length(.)) %>%
